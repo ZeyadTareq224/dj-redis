34bd1ede56a63263f97d292c2d2128b6ab43f1da
ZeyadTareq224 <ziyadtr101@gmail.com>
2023-03-28 14:01:43 +0200

    initial commit

---
+++ dj_redis/dj_redis/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from dj_redis.users.models import User
+from dj_redis.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
